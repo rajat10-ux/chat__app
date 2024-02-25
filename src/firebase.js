84bca02a17d8336f0060ab3370f0bf917bd42670
Rajat Kumar <103815726+rajat10-ux@users.noreply.github.com>
2024-02-25 18:13:29 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyCXRoF72umVCxSA6hHkAFK-Czc9D6P7mMQ",
+  authDomain: "chat-app-61180.firebaseapp.com",
+  projectId: "chat-app-61180",
+  storageBucket: "chat-app-61180.appspot.com",
+  messagingSenderId: "597802998431",
+  appId: "1:597802998431:web:1a67c534e7c6722e7320aa"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
